629768d881fed60bf9cc2e5f79ed9eabd94c1725
camilainf <camila.escobedo.f@mail.pucv.cl>
2022-09-23 19:39:38 -0300

    feat(routing): routing de los componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,21 @@ import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './pages/inicio/inicio.component';
 import { InicioSesionComponent } from './pages/inicio-sesion/inicio-sesion.component';
+import { PassRecoverComponent } from './pages/pass-recover/pass-recover.component';
+import { CreacioncuentaComponent } from './pages/creacioncuenta/creacioncuenta.component';
+import { MinutasComponent } from './pages/minutas/minutas.component';
+import { PerfilComponent } from './pages/perfil/perfil.component';
+import { InsumosComponent } from './pages/insumos/insumos.component';
 
 const routes: Routes = [
   {path:'',component: InicioComponent},
   {path:'inicio',component: InicioComponent},
-  {path:'iniciosesion', component: InicioSesionComponent}
+  {path:'iniciosesion', component: InicioSesionComponent},
+  {path:'passrecover', component: PassRecoverComponent},
+  {path:'creacioncuenta', component: CreacioncuentaComponent},
+  {path:'minutas', component: MinutasComponent},
+  {path:'perfil', component: PerfilComponent},
+  {path:'insumos', component: InsumosComponent},
 ];
 
 @NgModule({
@@ -14,3 +24,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
+ 
