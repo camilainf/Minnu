fdd7d82aec8972d311fa3c565904222f6355354d
camilainf <camila.escobedo.f@mail.pucv.cl>
2022-09-25 22:59:49 -0300

    cambiar estructura de regimenes y recetas

--- src/app/pages/regimenes/regimenes.component.ts
+++ src/app/pages/regimenes/regimenes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Regimen } from "../../services/regimen/regimen.type";
 import { RegimenService } from "../../services/regimen/regimen.services";
+import { Menu } from 'src/app/services/menu/menu.type';
+import { MenusService } from '../../services/menu/menus.service';
 
 @Component({
   selector: 'app-regimenes',
@@ -9,11 +11,13 @@ import { RegimenService } from "../../services/regimen/regimen.services";
 })
 export class RegimenesComponent implements OnInit {
   regimenes : Regimen[] = [];
+  menus : Menu[] = [];
 
-  constructor(private regimenService: RegimenService) { }
+  constructor(private regimenService: RegimenService, private menusService: MenusService) {}
 
   ngOnInit(): void {
     this.regimenes = this.regimenService.regimenes;
+    this.menus = this.menusService.menus;
   }
 
 }
--- src/app/services/regimen/regimen.type.ts
+++ src/app/services/regimen/regimen.type.ts
@@ -2,7 +2,7 @@ import { Receta } from '../receta/receta.type';
 import { Menu } from '../menu/menu.type';
 
 export interface Regimen {
-    nombre: string;
-    raciones: number;
-    menu: Menu;
+    tipo: string;
+    almuerzo: Menu;
+    cena: Menu;  
 }
