26fbf9a4953e07f6583a0b6094d678c69097fa71
Camila <camila.escobedo.f@mail.pucv.cl>
2022-10-29 19:49:19 -0300

    fix(inicio): modal y iconos en minutas

--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { InsumosService } from '../../services/insumo/insumos.service';
 import { MinutasService } from '../../services/minuta/minutas.service';
 import { Minuta } from '../../services/minuta/minuta.type';
-
+declare var window : any;
 
 @Component({
   selector: 'app-inicio',
@@ -11,6 +11,7 @@ import { Minuta } from '../../services/minuta/minuta.type';
   styleUrls: ['./inicio.component.scss']
 })
 export class InicioComponent implements OnInit {
+  formModal: any;
   insumos: Insumo[] = [];
   minutas : Minuta[] = [];
 
@@ -25,6 +26,17 @@ export class InicioComponent implements OnInit {
       this.insumos = data;
     })
     
+    this.formModal = new window.bootstrap.Modal(
+      document.getElementById('myModal')
+    )
+  }
+
+  openFormModal() {
+    this.formModal.show();
+  }
+
+  saveSomeThing() {
+    this.formModal.hide();
   }
 
 }
