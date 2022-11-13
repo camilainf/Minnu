fd25f8ed144eba1725e2bda3b452ffa18c748111
Camila Escobedo <camila.escobedo.f@mail.pucv.cl>
2022-11-13 00:31:18 -0300

    add(inicio): cargar y mappear insumos

--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -1,8 +1,10 @@
-import { Insumo } from 'src/app/services/insumo/insumo.type';
+import { Insumo, InsumoDTO } from 'src/app/services/insumo/insumo.type';
 import { Component, OnInit } from '@angular/core';
 import { InsumosService } from '../../services/insumo/insumos.service';
 import { MinutasService } from '../../services/minuta/minutas.service';
 import { Minuta } from '../../services/minuta/minuta.type';
+import { InsumoMapper } from 'src/app/services/insumo/insumo.mapper';
+
 declare var window : any;
 
 @Component({
@@ -13,17 +15,32 @@ declare var window : any;
 export class InicioComponent implements OnInit {
   formModal: any;
   insumos: Insumo[] = [];
+  insumo: Insumo = {} as Insumo;
   minutas : Minuta[] = [];
 
-  constructor(private insumosService: InsumosService,private minutasService: MinutasService) {}
+  constructor(private insumosService: InsumosService,private minutasService: MinutasService, private insumoMapper: InsumoMapper) {}
 
   ngOnInit(): void {
     this.minutasService.cargarMinutas().subscribe((data)=>{
       this.minutas = data;
     })
 
+    this.insumosService.cargarInsumoById().subscribe((data)=>{
+      console.log(data);    
+
+      this.insumo = this.insumoMapper.mapInsumoDTOToInsumo(data as InsumoDTO);
+
+    })
+
     this.insumosService.cargarInsumos().subscribe((data)=>{
-      this.insumos = data;
+      console.log(data);
+
+      for(let i in data) {
+        this.insumos.push(this.insumoMapper.mapInsumoDTOToInsumo(data[i] as InsumoDTO));
+      }
+
+      console.log(this.insumos);      
+
     })
     
     this.formModal = new window.bootstrap.Modal(
