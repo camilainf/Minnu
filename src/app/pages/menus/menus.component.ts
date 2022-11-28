67ef35a8e72d9091e9ef09dc2fa06ccb4ec016ea
Camila Escobedo <camila.escobedo.f@mail.pucv.cl>
2022-11-28 12:00:28 -0300

    fix(menus): modal ver detalles y agregar

--- src/app/pages/menus/menus.component.ts
+++ src/app/pages/menus/menus.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MenusService } from '../../services/menu/menus.service';
 import { Menu } from 'src/app/services/menu/menu.type';
+declare var window : any;
 
 @Component({
   selector: 'app-menus',
@@ -9,13 +10,53 @@ import { Menu } from 'src/app/services/menu/menu.type';
 })
 export class MenusComponent implements OnInit {
   menus: Menu[] = [];
+  formModal: any;
+  modalRegistroMenu : any;
 
   constructor(private menusService: MenusService) { }
 
   ngOnInit(): void {
+
     this.menusService.cargarMenus().subscribe((data)=>{
+      
       this.menus = data;
+
     })
+
+    this.formModal = new window.bootstrap.Modal(
+      document.getElementById('myModal')
+    )
+
+    this.modalRegistroMenu = new window.bootstrap.Modal(
+      document.getElementById('modalRegistroMenu')
+    )
+  }
+
+
+  // MODAL PARA VER DETALLES DE LA RECETA
+  openFormModal() {
+    this.formModal.show();
+  }
+
+  saveSomeThing() {
+    this.formModal.hide();
+  }
+
+  closeModal() {
+    this.formModal.hide();
+  }
+
+  // MODAL PARA REGISTRAR UN MENU
+  openRegistroModal() {
+    this.modalRegistroMenu.show();
+  }
+
+  cancelarRegistro() {
+    this.modalRegistroMenu.hide();
+  }
+
+  guardarCambiosRegistro() {
+    this.modalRegistroMenu.hide();
   }
 
 }
