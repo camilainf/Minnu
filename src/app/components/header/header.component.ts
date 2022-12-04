9a81aacfa07384e40fb863a306a1a584872df185
Camila Escobedo <camila.escobedo.f@mail.pucv.cl>
2022-12-04 11:48:51 -0300

    fix: admin view

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import {UserService} from "../../services/usuario/usuarios.service";
+import {UserIn} from "../../services/usuario/usuario.type";
 
 @Component({
   selector: 'app-header',
@@ -9,7 +11,7 @@ import { Router } from '@angular/router';
 export class HeaderComponent implements OnInit {
   public isCollapsed = true;
 
-  constructor(private router : Router) { }
+  constructor(private router : Router, private userService: UserService) { }
 
   ngOnInit(): void {
   }
@@ -22,5 +24,16 @@ export class HeaderComponent implements OnInit {
 
   loggedIn() {
     return localStorage.getItem('token');
+  }
+
+  esAdmin(): boolean {
+    if ( localStorage.getItem('user') ) {
+      let usuario: UserIn = JSON.parse(localStorage.getItem('user')!);
+
+      if ( usuario.isAdmin ) {
+        return true;
+      }
+    }
+    return false;
   }
 }
